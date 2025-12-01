30551dc9b43fdaede7d1cf9cb74a6f836a5df94a
chalapatiankammarao@gmail.com <chalapatiankammarao@gmail.com>
2025-12-01 11:57:22 +0530

    learn operators

---
+++ Pythonbasics/Operators.py
@@ -0,0 +1,25 @@
+x = 7
+y = 5
+######print(x<=y)
+#y =24
+#print(x+y)
+#(x-y)
+#print(x*y)
+#print(y/x)
+#print(y%x)
+#print(x**2)
+#print(x//2)
+
+
+a = [20, 30, 40]
+a = [10, 50, 60]
+#a = b
+print (a is a)
+#print (a is not b)
+
+
+fruits = ["apple", "banana", "cherry"]
+if "orange" in fruits:
+  print("Yes, orange is in the fruits list")
+else:
+    print("No, orange is not in the fruits list")
