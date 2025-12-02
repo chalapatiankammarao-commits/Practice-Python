90e345a0c332f9d1d3a8ecb48ffac63052f7c4bb
chalapatiankammarao@gmail.com <chalapatiankammarao@gmail.com>
2025-12-02 21:28:34 +0530

    tuples exercise

--- Pythonbasics/tuples.py
+++ Pythonbasics/tuples.py
@@ -15,14 +15,129 @@ c = a + b
 #print("apple")
 #print("banana")
 
+#fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
 
-fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+#(green, *tropic, red, blue) = fruits
 
-(green, *tropic, red, blue) = fruits
+#print(green)
+#(tropic)
+#print(red)
+#print(blue)
 
+#tuples
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal")
+print(mytuple)
+
+#allow duplicate
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal", "aravind", "sudheer")
+print(mytuple)
+#length of the tuple
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal")
+print(len(mytuple))
+
+#create tuple with one value
+
+mytuple = ("anand",)
+print(mytuple)
+
+mytuple = ("anand")
+print(mytuple)
+
+#access tuple items with indexing
+
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal")
+print(mytuple[2])
+
+#negative index
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal")
+print(mytuple[-4:-1])
+
+#check if items is exits
+
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal")
+if "aravind" in mytuple:
+    print("yes, 'aravind' in mytuple")
+
+#update tuple
+
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal")
+x = list(mytuple)
+x[2] = "sujatha"
+mytuple = tuple(x)
+print(mytuple)
+
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal")
+x = list(mytuple)
+x.append("govind")
+mytuple = tuple(x)
+print(mytuple)
+
+#tuple to tuple adding
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal")
+my_tuple = ("suraksha",)
+mytuple += my_tuple
+print(mytuple)
+
+#remove items in tuples
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal")
+x = list(mytuple)
+x.remove("gunashekar")
+mytuple = tuple(x)
+print(mytuple)
+
+#unpack tuples
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal")
+print("aravind",)
+print("gunashekar",)
+print("sudheer",)
+print("gopal",)
+
+#using asterisk*
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal")
+(green, yellow, *red) = mytuple
 print(green)
-print(tropic)
+print(yellow)
 print(red)
-print(blue)
+
+#join tuples
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal")
+my_tuple = ("gold", "silver", "platinum",)
+z = mytuple + my_tuple
+print(z)
+
+#multiply the tuples
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal")
+mynewtuple = mytuple*2
+print(mynewtuple)
+
+#tuple methods count, index
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal", "aravind")
+x = mytuple.count("aravind")
+print(x)
+
+mytuple = ("aravind", "gunashekar", "sudheer", "gopal", "aravind")
+x = mytuple.index("aravind")
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+ 
+
 
 
